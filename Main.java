0248e859de34df6d3d2b40e75d889ddc3d917884
Ahmed <76680009+AhmedRadwan02@users.noreply.github.com>
2023-05-02 23:24:51 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        Tokenizor tokenizer = new Tokenizor("textToken.txt");
+        Tokenizer tokenizer = new Tokenizer("textToken.txt");
         tokenizer.readTokens();
         
 	}
